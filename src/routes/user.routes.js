cbd6f750830b8beaef9311bde7b98e1a841a9670
sainidaksh45 <dakshsainisaini4@gmail.com>
2025-01-31 19:27:15 +0530

    ADD REGISTER CONTROLLER

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,23 @@
 import express from "express";
 import {Router} from "express";
 import {registerUser}from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js"; 
+
 
 
 const router=express.Router()
 
-router.post("/register", (req, res) => {
-    res.json({ message: "User registered successfully!" });
-  });
+router.route("/register").post(
+  upload.fields([
+{
+  name:"avatar",
+  maxCount:1
+},{
+  name:"coverImage",
+  maxCount:1
+}
+  ]),
+  registerUser
+);
 //router.route("/login").post(login)
 export default router
